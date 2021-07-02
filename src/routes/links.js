fe9cb3632ee3d793f3e0c5f0da1003a20c4e21e9
BetulioMay <cesarmay@protonmail.com>
2021-07-02 22:49:20 +0200

    DELETE links function added...

--- src/routes/links.js
+++ src/routes/links.js
@@ -3,6 +3,12 @@ const router = express.Router();
 
 const pool = require('../database'); // Reference to the database
 
+router.get('/', async (req, res) => {
+	const links = await pool.query('SELECT * FROM links');
+
+	res.render('links/list', {links});
+});
+
 router.get('/add', (req, res) => {
 	res.render('links/add');
 });
@@ -19,10 +25,10 @@ router.post('/add', async (req,res) => {
 	res.redirect('/links');
 });
 
-router.get('/', async (req, res) => {
-	const links = await pool.query('SELECT * FROM links');
-
-	res.render('links/list', {links});
+router.get('/delete/:id', async (req, res) => {
+	const { id } = req.params;
+	await pool.query('DELETE FROM links WHERE id=?', [id])
+	res.redirect('/links');
 });
 
 module.exports = router;
